e7240a7985ed20b4a3e874cdee1ad746baa80df9
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2023-04-30 15:43:25 +0900

    Bump up AGP to 8.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 
 android {
     compileSdkVersion(30)
-    buildToolsVersion = "30.0.2"
 
     defaultConfig {
         applicationId = "net.twinte.android"
@@ -34,6 +33,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    namespace = "net.twinte.android"
 }
 
 dependencies {
