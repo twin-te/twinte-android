ff8c3d5a80688a4a0ddf1216c2b82bf6578ac8d4
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2024-04-24 18:40:42 +0900

    Bump up twinte-android to 2.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "net.twinte.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 21
-        versionName = "2.1.1"
+        versionCode = 22
+        versionName = "2.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
